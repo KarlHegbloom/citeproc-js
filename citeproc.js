aa4010ce08b35b7b878367a8da50f9b198e3ff6b
Karl Hegbloom <karl.hegbloom@gmail.com>
2016-10-19 00:16:10 -0600

    Log the item.id with CSL STYLE ERROR so I can find the right citation!

--- citeproc.js
+++ citeproc.js
@@ -5468,10 +5468,11 @@ CSL.getCitationCluster = function (inputList, citationID) {
             if (this.tmp.has_purged_parallel) {
                 composite.push("");
             } else {
-                var errStr = "[CSL STYLE ERROR: reference with no printed form.]";
+                var errStr = "[CSL STYLE ERROR: reference with no printed form. (cite) itemID=" + error_object[itemID] + "]";
                 var preStr = pos === 0 ? txt_esc(this.citation.opt.layout_prefix) : "";
                 var sufStr = pos === (myblobs.length - 1) ? txt_esc(this.citation.opt.layout_suffix) : "";
                 composite.push(preStr + errStr + sufStr);
+                console.log("\n" + errStr + "\n");
             }
         }
         if (buffer.length && "string" === typeof composite[0]) {
@@ -5898,7 +5899,8 @@ CSL.getBibliographyEntries = function (bibsection) {
         }
         res = this.output.string(this, this.output.queue)[0];
         if (!res) {
-            res = "\n[CSL STYLE ERROR: reference with no printed form.]\n";
+            res = "\n[CSL STYLE ERROR: reference with no printed form. (bib) item.id=" + JSON.stringify(item.id) + "]\n";
+            console.log(res);
         }
         ret.push(res);
     }
--- src/api_bibliography.js
+++ src/api_bibliography.js
@@ -321,7 +321,9 @@ CSL.getBibliographyEntries = function (bibsection) {
         res = this.output.string(this, this.output.queue)[0];
         
         if (!res) {
-            res = "\n[CSL STYLE ERROR: reference with no printed form.]\n";
+            res = "\n[CSL STYLE ERROR: reference with no printed form. (bib) item.id=" +
+                item.id + " item.system_id=" + item.system_id + "]\n";
+            console.log(res);
         }
         ret.push(res);
     }
